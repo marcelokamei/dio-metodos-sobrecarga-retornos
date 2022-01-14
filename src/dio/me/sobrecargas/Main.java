5c1b9d0da16555e212aa6d104eff778423702cc9
marcelokamei <marcelokamei@hotmail.com>
2022-01-14 01:12:04 -0400

    Sobrecarga

---
+++ src/dio/me/sobrecargas/Main.java
@@ -0,0 +1,13 @@
+package dio.me.sobrecargas;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //quadrilatero
+        System.out.println("Quadrilátero");
+        Quadrilatero.area(9);
+        Quadrilatero.area(9,7);
+        Quadrilatero.area(9,8,12);
+
+    }
+}
